7146f33a3ad8bb2e82e19afc7b1b99ff86c2d490
MYXAMOR338 <silova155@gmail.com>
2024-03-26 21:51:30 +0300

    Убрал метаданные добавил поиск параграфов

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ max_tokens = int(config['API']['max_tokens'])
 temp = int(config['API']['temp'])
 model = config['API']['model']
 
-
+result_1 = ""
+result_2 = ""
+result_3 = ""
 # Функция для кодирования изображения в формат Base64
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
@@ -29,6 +31,7 @@ def pause_for_two_hours():
     print("Превышен лимит запросов. Остановка работы на два часа.")
     time.sleep(7200)  # 7200 секунд = 2 часа
     
+print("Выберите папку с вашими изображениями")
 # Путь к папке с изображениями
 folder_path = filedialog.askdirectory(title="Выберите папку с изображениями")
 
@@ -92,57 +95,60 @@ for image_file in image_files:
 
             # Получаем текстовый ответ от GPT
             gpt_response = response.choices[0]["message"]["content"]
-            description = ""
-            # Ищем строки с ключевыми словами "Title:" и "Tags:"
-            title_line = next((line[len("Title:"):].strip() for line in gpt_response.split('\n') if line.startswith('Title:')), None)
-            tags_line = next((line[len("Tags:"):].strip() for line in gpt_response.split('\n') if line.startswith('Tags:')), None)
 
+
+            paragraphs = gpt_response.split("\n\n")
+            result_1 = paragraphs[0]
+            result_2 = paragraphs[1]
+            result_3 = paragraphs[2]
+
+
+            if result_1:
+                print("Найден параграф 1:")
+            if result_2:
+                print("Найден параграф 2:")
+            if result_3:
+                print("Найден параграф 3:")
+                
             # Выводим информацию о тегах и названии файла
             print(f"File: '{image_file}' Обработан ключом: {file_count}! \n{response.choices[0]['message']['content']}")
             print("---------------------------------------")
 
-            if tags_line:
-                print("Найдены теги:", tags_line, "\n")
-            if title_line:
-                title_index = gpt_response.index(title_line)
-                description = gpt_response[title_index + len(title_line):].strip()
-                print("Найдено название:", title_line)
-                if description:
-                    print("Найдено описание:", description, '\n')
 
-            # Проверяем наличие запрещенных слов
-            if title_line and description and ("the photo shows" in title_line or "In the picture" in title_line or "the photo shows" in description or "In the picture" in description):
-                print("Ошибка: найдены запрещенные слова в ответе")
-                attempts += 1  # Увеличиваем счетчик попыток
-                continue  # Повторяем обработку файла
+
+            # # Проверяем наличие запрещенных слов
+            # if title_line and description and ("the photo shows" in title_line or "In the picture" in title_line or "the photo shows" in description or "In the picture" in description):
+            #     print("Ошибка: найдены запрещенные слова в ответе")
+            #     attempts += 1  # Увеличиваем счетчик попыток
+            #     continue  # Повторяем обработку файла
 
             # Загружаем изображение
-            img = Image.open(image_path)
+            # img = Image.open(image_path)
 
             # Проверяем наличие метаданных Exif
             # Проверяем наличие метаданных Exif
 
-            exif_dict = {"0th": {}, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": None}
+            # exif_dict = {"0th": {}, "Exif": {}, "GPS": {}, "Interop": {}, "1st": {}, "thumbnail": None}
 
-            # Кодировка строк в UTF-16LE для совместимости с XP тегами
-            if title_line:
-                encoded_title = title_line.encode('utf-16le')
-            if description:
-                encoded_description = description.encode('utf-16le')
-            if tags_line:
-                encoded_tags = tags_line.encode('utf-16le')
+            # # Кодировка строк в UTF-16LE для совместимости с XP тегами
+            # if title_line:
+            #     encoded_title = title_line.encode('utf-16le')
+            # if description:
+            #     encoded_description = description.encode('utf-16le')
+            # if tags_line:
+            #     encoded_tags = tags_line.encode('utf-16le')
 
-            # Добавление метаданных
-            if title_line:
-                exif_dict['0th'][piexif.ImageIFD.XPTitle] = encoded_title
-            if description:
-                exif_dict['0th'][piexif.ImageIFD.XPSubject] = encoded_description
-            if tags_line:
-                exif_dict['0th'][piexif.ImageIFD.XPKeywords] = encoded_tags
+            # # Добавление метаданных
+            # if title_line:
+            #     exif_dict['0th'][piexif.ImageIFD.XPTitle] = encoded_title
+            # if description:
+            #     exif_dict['0th'][piexif.ImageIFD.XPSubject] = encoded_description
+            # if tags_line:
+            #     exif_dict['0th'][piexif.ImageIFD.XPKeywords] = encoded_tags
 
             # Сохранение изменений
-            exif_bytes = piexif.dump(exif_dict)
-            img.save(image_path, exif=exif_bytes, format='JPEG')
+            # exif_bytes = piexif.dump(exif_dict)
+            # img.save(image_path, exif=exif_bytes, format='JPEG')
 
         except Exception as e:
             print("Ошибка при обработке файла:", e)
