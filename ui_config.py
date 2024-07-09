68ce85add0bb93167a3133598bb5648b2f1e71b8
MYXAMOR338 <silova155@gmail.com>
2024-07-09 16:09:16 +0300

    no imagine dev in gui

--- ui_config.py
+++ ui_config.py
@@ -22,7 +22,7 @@ class Ui_Form(object):
     def setupUi(self, Form):
         if not Form.objectName():
             Form.setObjectName(u"Form")
-        Form.resize(1182, 720)
+        Form.resize(1161, 474)
         Form.setMaximumSize(QSize(16777215, 720))
         self.gridLayout = QGridLayout(Form)
         self.gridLayout.setObjectName(u"gridLayout")
@@ -67,76 +67,6 @@ class Ui_Form(object):
         self.label_11.setFont(font)
 
         self.verticalLayout.addWidget(self.label_11)
-
-        self.label_12 = QLabel(Form)
-        self.label_12.setObjectName(u"label_12")
-        self.label_12.setMaximumSize(QSize(16777215, 30))
-        self.label_12.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_12)
-
-        self.label_13 = QLabel(Form)
-        self.label_13.setObjectName(u"label_13")
-        self.label_13.setMaximumSize(QSize(16777215, 30))
-        self.label_13.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_13)
-
-        self.label_14 = QLabel(Form)
-        self.label_14.setObjectName(u"label_14")
-        self.label_14.setMaximumSize(QSize(16777215, 30))
-        self.label_14.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_14)
-
-        self.label_15 = QLabel(Form)
-        self.label_15.setObjectName(u"label_15")
-        self.label_15.setMaximumSize(QSize(16777215, 30))
-        self.label_15.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_15)
-
-        self.label_16 = QLabel(Form)
-        self.label_16.setObjectName(u"label_16")
-        self.label_16.setMaximumSize(QSize(16777215, 30))
-        self.label_16.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_16)
-
-        self.label_17 = QLabel(Form)
-        self.label_17.setObjectName(u"label_17")
-        self.label_17.setMaximumSize(QSize(16777215, 30))
-        self.label_17.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_17)
-
-        self.label_18 = QLabel(Form)
-        self.label_18.setObjectName(u"label_18")
-        self.label_18.setMaximumSize(QSize(16777215, 30))
-        self.label_18.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_18)
-
-        self.label_19 = QLabel(Form)
-        self.label_19.setObjectName(u"label_19")
-        self.label_19.setMaximumSize(QSize(16777215, 30))
-        self.label_19.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_19)
-
-        self.label_20 = QLabel(Form)
-        self.label_20.setObjectName(u"label_20")
-        self.label_20.setMaximumSize(QSize(16777215, 30))
-        self.label_20.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_20)
-
-        self.label_21 = QLabel(Form)
-        self.label_21.setObjectName(u"label_21")
-        self.label_21.setMaximumSize(QSize(16777215, 30))
-        self.label_21.setFont(font)
-
-        self.verticalLayout.addWidget(self.label_21)
 
         self.label_2 = QLabel(Form)
         self.label_2.setObjectName(u"label_2")
@@ -227,76 +157,6 @@ class Ui_Form(object):
 
         self.verticalLayout_2.addWidget(self.lineEdit_11)
 
-        self.lineEdit_12 = QLineEdit(Form)
-        self.lineEdit_12.setObjectName(u"lineEdit_12")
-        self.lineEdit_12.setMinimumSize(QSize(0, 32))
-        self.lineEdit_12.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_12)
-
-        self.lineEdit_13 = QLineEdit(Form)
-        self.lineEdit_13.setObjectName(u"lineEdit_13")
-        self.lineEdit_13.setMinimumSize(QSize(0, 32))
-        self.lineEdit_13.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_13)
-
-        self.lineEdit_14 = QLineEdit(Form)
-        self.lineEdit_14.setObjectName(u"lineEdit_14")
-        self.lineEdit_14.setMinimumSize(QSize(0, 32))
-        self.lineEdit_14.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_14)
-
-        self.lineEdit_15 = QLineEdit(Form)
-        self.lineEdit_15.setObjectName(u"lineEdit_15")
-        self.lineEdit_15.setMinimumSize(QSize(0, 32))
-        self.lineEdit_15.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_15)
-
-        self.lineEdit_16 = QLineEdit(Form)
-        self.lineEdit_16.setObjectName(u"lineEdit_16")
-        self.lineEdit_16.setMinimumSize(QSize(0, 32))
-        self.lineEdit_16.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_16)
-
-        self.lineEdit_17 = QLineEdit(Form)
-        self.lineEdit_17.setObjectName(u"lineEdit_17")
-        self.lineEdit_17.setMinimumSize(QSize(0, 32))
-        self.lineEdit_17.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_17)
-
-        self.lineEdit_18 = QLineEdit(Form)
-        self.lineEdit_18.setObjectName(u"lineEdit_18")
-        self.lineEdit_18.setMinimumSize(QSize(0, 32))
-        self.lineEdit_18.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_18)
-
-        self.lineEdit_19 = QLineEdit(Form)
-        self.lineEdit_19.setObjectName(u"lineEdit_19")
-        self.lineEdit_19.setMinimumSize(QSize(0, 32))
-        self.lineEdit_19.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_19)
-
-        self.lineEdit_20 = QLineEdit(Form)
-        self.lineEdit_20.setObjectName(u"lineEdit_20")
-        self.lineEdit_20.setMinimumSize(QSize(0, 32))
-        self.lineEdit_20.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_20)
-
-        self.lineEdit_21 = QLineEdit(Form)
-        self.lineEdit_21.setObjectName(u"lineEdit_21")
-        self.lineEdit_21.setMinimumSize(QSize(0, 32))
-        self.lineEdit_21.setFont(font1)
-
-        self.verticalLayout_2.addWidget(self.lineEdit_21)
-
         self.lineEdit_22 = QLineEdit(Form)
         self.lineEdit_22.setObjectName(u"lineEdit_22")
         self.lineEdit_22.setMinimumSize(QSize(0, 32))
@@ -365,16 +225,6 @@ class Ui_Form(object):
         self.label_9.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 GPT (3):", None))
         self.label_10.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 GPT (4):", None))
         self.label_11.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 GPT (5):", None))
-        self.label_12.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (1):", None))
-        self.label_13.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (2):", None))
-        self.label_14.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (3):", None))
-        self.label_15.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (4):", None))
-        self.label_16.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (5):", None))
-        self.label_17.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (6):", None))
-        self.label_18.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (7):", None))
-        self.label_19.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (8):", None))
-        self.label_20.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (9):", None))
-        self.label_21.setText(QCoreApplication.translate("Form", u"\u041a\u043b\u044e\u0447 MIdjourney (10):", None))
         self.label_2.setText(QCoreApplication.translate("Form", u"\u041f\u0440\u043e\u043c\u0442:", None))
         self.label_3.setText(QCoreApplication.translate("Form", u"\u041a\u043e\u043b\u0438\u0447\u0435\u0441\u0442\u0432\u043e \u043f\u043e\u043f\u044b\u0442\u043e\u043a:", None))
         self.label_4.setText(QCoreApplication.translate("Form", u"\u0420\u0435\u0436\u0438\u043c \u0440\u0430\u0431\u043e\u0442\u044b:", None))
